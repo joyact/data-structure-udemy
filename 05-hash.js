86be5117d0ecbc1afc44949b2465c14f111b04a4
joyAct <joyleeact@gmail.com>
2020-10-25 23:36:25 +0900

    Add a keys() method in hash.js

--- 05-hash.js
+++ 05-hash.js
@@ -1,29 +1,39 @@
 class HashTable {
-  constructor(size = 4) {
+  constructor(size = 53) {
     this.keyMap = new Array(size);
   }
 
   _hash(key) {
     let total = 0;
-    let WEIRD_PRIME = 31;
+    let PRIME_NUM = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
       let value = char.charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+      total = (total * PRIME_NUM + value) % this.keyMap.length;
     }
     return total;
   }
 
   set(key, value) {
+    /*
+      Accepts a key and a value
+      Hashes the key
+      Stores the key-value pair in the hash table array via separate chaining
+      */
     let index = this._hash(key);
     if (!this.keyMap[index]) {
       this.keyMap[index] = [];
     }
     this.keyMap[index].push([key, value]);
-    console.log(this.keyMap);
   }
 
   get(key) {
+    /*
+      Accepts a key
+      Hashes the key
+      Retrieves the key-value pair in the hash table
+      If the key isn't found, returns undefined
+    */
     let index = this._hash(key);
     if (this.keyMap[index]) {
       for (let i = 0; i < this.keyMap[index].length; i++) {
@@ -34,11 +44,22 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    /*
+     Loops through the hash table array 
+     Returns an array of keys in the table
+    */
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
 }
-
-let ht = new HashTable();
-
-ht.set('hello', 'goodbye!');
-ht.set('gfd', 's!');
-ht.set('b', 'c!');
-console.log(ht);
