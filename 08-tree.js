c4da78c016cabf92cb98f4aebd2697df1ec34152
joyAct <joyleeact@gmail.com>
2020-10-30 22:21:40 +0900

    Add insert() in tree.js

--- 08-tree.js
+++ 08-tree.js
@@ -10,6 +10,43 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    // tree is empty, insert to root!
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    // tree has nodes, let's move
+    let current = this.root;
+    while (current) {
+      // handle duplicates
+      if (value === current.value) return undefined;
+
+      // smaller value, go to left side
+      if (value < current.value) {
+        // It's empty, insert!
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        // not empty, keep moving.
+        current = current.left;
+
+        // bigger value, go to right side
+      } else {
+        // It's empty, insert!
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        // not empty, keep moving.
+        current = current.right;
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree();
